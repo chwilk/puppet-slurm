efbfcc3b574332bba7157b9fc66bb314847cdbc1
chwilk <chwilk@gmail.com>
2016-01-27 16:18:40 -0600

    Added rest of SLURM specific configs

--- manifests/config.pp
+++ manifests/config.pp
@@ -19,6 +19,74 @@ class slurm::config inherits slurm {
             group => "munge",
             mode => "0400",
         }
-
     }
+
+    # PAM
+    unless $slurm::disable_pam {
+        case $::operatingsystem {
+            'RedHat': {
+                if $::operatingsystemmajrelease >= 7 { # sshd is going to password-auth in 7 and above.
+                    file_line { 'slurm_pam_password_auth_suff_access':
+                        path => '/etc/pam.d/password-auth',
+                        line => 'account  sufficient     pam_access.so',
+                        match => 'pam_access.so',
+                    }
+
+                    file_line { 'slurm_pam_password_auth_req_slurm':
+                        path => '/etc/pam.d/password-auth',
+                        line => 'account  required     pam_slurm.so',
+                        after => 'pam_access.so',
+                    }
+                }
+                file_line { 'slurm_pam_system_auth_suff_access':
+                    path => '/etc/pam.d/system-auth-ac',
+                    line => 'account  sufficient     pam_access.so',
+                    match => 'pam_access.so',
+                }
+
+                file_line { 'slurm_pam_system_auth_req_slurm':
+                    path => '/etc/pam.d/system-auth-ac',
+                    line => 'account  required     pam_slurm.so',
+                    after => 'pam_access.so',
+                }
+                
+            }
+        }
+    }
+    # SLURM
+    file { '/etc/default/slurmd':
+        ensure => file,
+        owner => 'root',
+        group => 'root',
+        mode => '0644',
+        content => "ulimit -l unlimited\n",
+    }
+
+    file {'/var/spool/slurm':
+        ensure => directory,
+        owner => "root",
+        group => "root",
+        mode => "0755",
+    }
+
+    file {'/var/spool/slurm/logs':
+        ensure => directory,
+        owner => "root",
+        group => "root",
+        mode => "0755",
+    }
+
+    file {'/var/run/slurm':
+        ensure => directory,
+        owner => "root",
+        group => "root",
+        mode => "0755",
+    }
+    
+    file {'/etc/slurm':
+        ensure => link,
+        target => $slurm::slurm_conf_location,
+        force  => true,
+    }
+
 }
