c14becfdb6902595c891b50cd847c6915c3583fd
chwilk <chwilk@gmail.com>
2017-07-17 21:23:53 -0500

    Clearing lint warnings

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,17 +1,17 @@
 class slurm::service inherits slurm {
   unless $slurm::disable_slurmd {
     service {$slurm::slurm_service_name:
-      ensure => running,
-      enable => true,
-      hasstatus => true,
+      ensure     => running,
+      enable     => true,
+      hasstatus  => true,
       hasrestart => true,
     }
   }
   unless $slurm::disable_munge {
     service {'munge':
-      ensure => running,
-      enable => true,
-      hasstatus => true,
+      ensure     => running,
+      enable     => true,
+      hasstatus  => true,
       hasrestart => true,
     }
   }
