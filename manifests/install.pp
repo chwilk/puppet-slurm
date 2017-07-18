b376d4135f5189b827383ba7ee154d650eb593fe
chwilk <chwilk@gmail.com>
2017-07-17 21:27:54 -0500

    Indentation fix

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,18 +1,18 @@
 class slurm::install inherits slurm {
 
-    if $slurm::package_manage {
-        unless $slurm::disable_munge {
-            package {$slurm::munge_packages:
-                ensure => $slurm::package_ensure,
-            }
-        }
-        unless $slurm::disable_pam {
-            package {$slurm::pam_packages:
-                ensure => $slurm::package_ensure,
-            }
-        }
-        package { $slurm::slurm_packages:
-            ensure => $slurm::package_ensure,
-        }
+  if $slurm::package_manage {
+    unless $slurm::disable_munge {
+      package {$slurm::munge_packages:
+        ensure => $slurm::package_ensure,
+      }
     }
+    unless $slurm::disable_pam {
+      package {$slurm::pam_packages:
+        ensure => $slurm::package_ensure,
+      }
+    }
+    package { $slurm::slurm_packages:
+      ensure => $slurm::package_ensure,
+    }
+  }
 }
