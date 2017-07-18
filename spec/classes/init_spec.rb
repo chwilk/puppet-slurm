020ddef8c07206c5816d70308b1371647d555a7c
chwilk <chwilk@gmail.com>
2017-07-17 23:39:57 -0500

    Included os iteration

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,18 @@
 require 'spec_helper'
 describe 'slurm' do
 
-  context 'with defaults for all parameters' do
-    it { should contain_class('slurm') }
+  on_supported_os.select { |_, f| f[:os]['family'] != 'Solaris' }.each do |os, f|
+    context "on #{os}" do
+      let(:facts) do
+        f.merge(super())
+      end
+      it { is_expected.to compile.with_all_deps }
+      describe "Testing dependencies between classes" do
+        it { should contain_class('slurm::install') }
+        it { should contain_class('slurm::config') }
+        it { should contain_class('slurm::service') }
+        it { is_expected.to contain_class('slurm::install').that_comes_before('Class[slurm::config]') }
+      end
+    end
   end
 end
