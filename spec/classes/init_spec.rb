1020924494756534563d9f9970de7c9f1b6673be
chwilk <chwilk@gmail.com>
2017-07-18 07:23:19 -0500

    Error on no super class...

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -3,9 +3,6 @@ describe 'slurm' do
 
   on_supported_os.select { |_, f| f[:os]['family'] != 'Solaris' }.each do |os, f|
     context "on #{os}" do
-      let(:facts) do
-        f.merge(super())
-      end
       it { is_expected.to compile.with_all_deps }
       describe "Testing dependencies between classes" do
         it { should contain_class('slurm::install') }
